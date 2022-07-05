6d30b65a261c9abf9efaf10381fa0d8edf73958a
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-07-05 08:55:15 -0300

    STATIC_ROOT e collectstatic

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -120,8 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / '',
+    BASE_DIR / 'base_static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
