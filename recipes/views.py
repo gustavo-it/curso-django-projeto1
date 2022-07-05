452acdf2693496b3e8a0a5c9821b1d18be54de66
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-07-05 15:16:10 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe  # importando a função
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
     #  Passando o caminho do template, lembre que por padrão o Django identifica a pasta templates
+    #  Estamos passando no contexto a função de teste, executando uma list comprehension que gera 10 receitas
 
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
+    #  Estamos passando a chave no singular, para que seja apresentado somente uma receita
