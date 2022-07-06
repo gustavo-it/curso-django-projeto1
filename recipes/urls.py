56918fb418b4443469e3078bead0447bc1a80f70
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-07-06 10:36:52 -0300

    Nome único nas URLs e app_name para Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 # Da pasta em que eu estou importe as views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # importando a view home
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name="home"),  # importando a view home
+    path('recipes/<int:id>/', views.recipes, name="recipe"),
 ]
