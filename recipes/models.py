69e4fb5d2d74637ab3bd04485d53f18668efe293
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-07-06 13:49:50 -0300

    Primeiro Django Model e seus atributos

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Um model nada mais é que uma class. Precisamos dizer que esta class herda de models.Model
+    Nós criamos as colunas como sendo atributos.
+    """
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()  # Atributo slug específico para o slug
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    serving_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(
+        default=False)  # definindo um valor padrão
+    created_at = models.DateTimeField(auto_now_add=True)
+    # auto_now_add gera uma data/hora automaticamente na hora da criação e esta não é alterada
+    update_at = models.DateTimeField(auto_now=True)
+    # auto_now gera a data/hora sempre que o elemento é atualizado. Ele só é chamado quando  o registro
+    # for atualizado
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    """
+    Estamos utilizando o campo ImageField (para subir uma imagem).
+    Este campo recebe o valor upload_to (que é o caminho para onde a imagem irá ficará salva).
+    Ou seja, ficará salvo na pasta recipes > cover > pasta com o nome (ano/mes/dia).
+    """
