0544d5fd267ae70dabe9b9aff3b40dc26a70dd59
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-09-05 16:11:38 -0300

    A função render e mais sobre caminhos de templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recipes.views import home
+from recipes.views import contato, home
 
 urlpatterns = [
     path('', home), # Chamando a view home
+    path('contato/', contato)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Gustavo',
+    })
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
