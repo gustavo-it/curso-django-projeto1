37955abecb8e0abb1408f51eb4a537b4afe7186d
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-09-14 10:46:11 -0300

    Primeiro Django Model e seus atributos

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.Charfield(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.Charfield(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
