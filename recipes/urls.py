be73e3ee830f7e587b5885686a4eef4788c30779
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-09-15 20:12:05 -0300

    Usando o model Recipe real (com QuerySet) na view home

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,7 @@ app_name = "recipes"
 
 urlpatterns = [
     path('', views.home, name="home"),  # Chamando a view home
-    path('recipes/<int:id>/', views.recipe, name="recipe")
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
