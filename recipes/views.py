241ef04978268fd4210ae6c06a5e684e31c91b6a
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-09-17 12:26:35 -0300

    Usando o shortcut get_list_or_404 (atalho)

--- recipes/views.py
+++ recipes/views.py
@@ -13,8 +13,11 @@ def home(request):
 
 def category(request, category_id):
     recipes = get_list_or_404(
-        Recipe, category__id=category_id, is_published=True).order_by("-id")
-
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by("-id")
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category │',
