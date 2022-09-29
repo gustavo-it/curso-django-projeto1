e0913bd2de7923c9d0bf687fb209bb2ad0cec60a
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-09-29 10:45:18 -0300

    Testando o context e content da response no template

--- recipes/test/test_recipe_views.py
+++ recipes/test/test_recipe_views.py
@@ -45,7 +45,13 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
+        response = self.client.get(reverse("recipes:home"))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe title', content)
+        self.assertIn('Recipe Description', content)
+        self.assertEqual(len(response_context_recipes), 1)
+        pass
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse("recipes:category", kwargs={"category_id": 1}))
