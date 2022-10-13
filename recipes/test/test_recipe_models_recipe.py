85a2286e669ad0ead1425925a2b016dadb4b24d9
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-10-13 10:19:38 -0300

    Testando max_lenght do model usando self.assertRaises (Context Manager)

--- recipes/test/test_recipe_models_recipe.py
+++ recipes/test/test_recipe_models_recipe.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 from .test_recipe_base import RecipeTestBase
 
 
@@ -5,3 +7,12 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        """
+        Testa se o título tem mais de 65 caracteres.
+        """
+        self.recipe.title = 'A' * 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()  # Aqui acontece a validação
