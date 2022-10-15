3c9a4ce6492e584ac8504c1a28c3be5866d6ef4b
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-10-15 12:37:42 -0300

    Testando a string representation da Recipe

--- recipes/test/test_recipe_models_recipe.py
+++ recipes/test/test_recipe_models_recipe.py
@@ -10,6 +10,9 @@ class RecipeModelTest(RecipeTestBase):
         return super().setUp()
 
     def make_recipe_no_defaults(self):
+        """
+        Criando uma receita específica para os nossos testes a seguir.
+        """
         recipe = Recipe(
             category=self.make_category(name="Comida árabe"),
             author=self.make_author(username="Novo usuário"),
@@ -33,16 +36,38 @@ class RecipeModelTest(RecipeTestBase):
         ('servings_unit', 65),
     ])
     def test_recipe_fields_max_length(self, field, max_length):
+        """
+        Esste test verifica se ocorre o ValidationError quando
+        tentamos cadastrar valores maiores que o permitido.
+        """
         setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
 
     def test_recipe_preparation_steps_is_html_if_false_by_default(self):
+        """
+        Verifica se de fato o "campo preparations_steps_is_html"
+        recebe como valor padrão o False.
+        """
         recipe = self.make_recipe_no_defaults()
         self.assertFalse(recipe.preparation_steps_is_html,
                          msg="Recipe preparation_steps_is_html is not False")
 
     def test_recipe_is_published_is_false_by_default(self):
+        """
+        Verifica se de fato o campo "is_published" recebe como valor
+        padrão o False.
+        """
         recipe = self.make_recipe_no_defaults()
         self.assertFalse(recipe.is_published,
                          msg="Recipe is_published is not False")
+
+    def test_recipe_string_representation(self):
+        """
+        Verifica se o método __str__ que utilizamos no models
+        retorna o título correto da receita.
+        """
+        self.recipe.title = "Testing Representation"
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), 'Testing Representation')
