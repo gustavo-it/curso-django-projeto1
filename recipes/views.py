a589fbd2e0d68c720bf43f57cc6cf3ea1044976b
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-10-21 13:41:39 -0300

    Adicionando o paginator na home

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -7,9 +8,15 @@ from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+
+    current_page = request.GET.get("page", 1)
+    paginator = Paginator(recipes, 9)
+    page_object = paginator.get_page(current_page)
+    return render(request,
+                  'recipes/pages/home.html',
+                  context={
+                      'recipes': page_object,
+                  })
 
 
 def category(request, category_id):
@@ -17,22 +24,24 @@ def category(request, category_id):
         Recipe.objects.filter(
             category__id=category_id,
             is_published=True,
-        ).order_by("-id")
-    )
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category │',
-    })
+        ).order_by("-id"))
+    return render(request,
+                  'recipes/pages/category.html',
+                  context={
+                      'recipes': recipes,
+                      'title': f'{recipes[0].category.name} - Category │',
+                  })
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(
-        Recipe, pk=id, is_published=True)
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-        'title': f"{recipe.title} |"
-    })
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    return render(request,
+                  'recipes/pages/recipe-view.html',
+                  context={
+                      'recipe': recipe,
+                      'is_detail_page': True,
+                      'title': f"{recipe.title} |"
+                  })
 
 
 def search(request):
@@ -41,12 +50,10 @@ def search(request):
     if not search_term:
         raise Http404()
 
-    recipes = Recipe.objects.filter(
-        Q(Q(title__icontains=search_term) |
-          Q(description__icontains=search_term),
-          ),
-        is_published=True
-    ).order_by("-id")
+    recipes = Recipe.objects.filter(Q(
+        Q(title__icontains=search_term)
+        | Q(description__icontains=search_term), ),
+                                    is_published=True).order_by("-id")
 
     return render(request, "recipes/pages/search.html", {
         "page_title": f"Search for '{search_term}' |",
