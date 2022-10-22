70a7cc5dc943ffd08ba66ef7401d742c25bb942e
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-10-22 18:51:14 -0300

    Adicionando paginação no template

--- recipes/views.py
+++ recipes/views.py
@@ -2,20 +2,30 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination_range
 
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    try:
+        current_page = int(request.GET.get("page", 1))
+    except ValueError:
+        current_page = 1
 
-    current_page = request.GET.get("page", 1)
     paginator = Paginator(recipes, 9)
     page_object = paginator.get_page(current_page)
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
     return render(request,
                   'recipes/pages/home.html',
                   context={
                       'recipes': page_object,
+                      'pagination_range': pagination_range,
                   })
 
 
@@ -53,7 +63,7 @@ def search(request):
     recipes = Recipe.objects.filter(Q(
         Q(title__icontains=search_term)
         | Q(description__icontains=search_term), ),
-                                    is_published=True).order_by("-id")
+        is_published=True).order_by("-id")
 
     return render(request, "recipes/pages/search.html", {
         "page_title": f"Search for '{search_term}' |",
