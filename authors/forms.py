e27243ec150801efb49fdee5562ffa3d76ce0917
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-01 20:13:51 -0300

    Sobrescrevendo campos do formulário completamente

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,28 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, "")
+    field.widget.attrs[attr_name] = f"{existing_attr} {attr_new_val}".strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs["placeholder"] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields["username"], "Your username")
+        add_placeholder(self.fields["email"], "Your email")
+        add_placeholder(self.fields["first_name"], "Ex: Maria")
+        add_placeholder(self.fields["last_name"], "Ex: Doe")
+
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "Repeat your password",
+        }
+    ))
 
     class Meta:
         model = User
@@ -33,11 +54,11 @@ class RegisterForm(forms.ModelForm):
         }
 
         widgets = {
-            "first_name": forms.TextInput(attr={
+            "first_name": forms.TextInput(attrs={
                 "placeholder": "type your first name here",
                 "class": "input text-input"
             }),
-            "password": forms.PasswordInput(attr={
+            "password": forms.PasswordInput(attrs={
                 "placeholder": "Type your password here"
             })
         }
