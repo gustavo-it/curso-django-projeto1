f27f3156230bf91d3c6a0f42815c284dbb34a4e0
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-23 15:07:48 -0300

    View, url para a criação de novas receitas

--- authors/views.py
+++ authors/views.py
@@ -146,3 +146,30 @@ def dashboard_recipe_edit(request, id):
             'form': form,
         }
     )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorsRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe: Recipe = form.save(commit=False)
+
+        recipe.authors = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Salvo com sucesso. Aguarde um ' +
+                         'administrador aprovar a sua receita.')
+        return redirect(reverse('authors:dashboard_recipe_edit',
+                                args=(recipe.id,)))
+
+    return render(request, 'authors/pages/dashboard_recipe.html', context={
+        'form': form,
+        'form_action': reverse('authors:dashboard_recipe_new')
+    })
