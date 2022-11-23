c6e27c6422e1ca27aa972cf023982f4039f2db80
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-23 10:20:59 -0300

    Toques finais no layout do formulário de criação de receitas

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -16,3 +16,25 @@ class AuthorsRecipeForm(forms.ModelForm):
         fields = ('title', 'description', 'preparation_time',
                   'preparation_time_unit', 'servings', 'servings_unit',
                   'preparation_steps', 'cover')
+
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('Pessoas', 'Pessoas'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Horas', 'Horas'),
+                    ('Minutos', 'Minutos'),
+                    ('Dias', 'Dias'),
+                )
+            )
+        }
