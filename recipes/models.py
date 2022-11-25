8e781fa32530792dbba7d2c5e568cdd9f0484309
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-25 20:19:30 -0300

    Fazendo “slugify” no model e usando “save” (extra dois sobre models)

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils.crypto import get_random_string
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -35,3 +37,12 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'
+            while Recipe.objects.filter(slug=slug).exists():
+                slug = slug + '-' + get_random_string(length=4)
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
