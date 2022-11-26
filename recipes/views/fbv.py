8f83ea74ef079e05b0c8e2048a2a2404d2ec9ab0
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-26 17:19:23 -0300

    Algumas remoções

--- recipes/views/fbv.py
+++ recipes/views/fbv.py
@@ -2,43 +2,12 @@ import os
 
 from django.db.models import Q
 from django.http.response import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
 
 PER_PAGE = int(os.environ.get("PER_PAGE", 6))
-
-
-def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
-
-    page_object, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request,
-                  'recipes/pages/home.html',
-                  context={
-                      'recipes': page_object,
-                      'pagination_range': pagination_range,
-                  })
-
-
-def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True,
-        ).order_by("-id"))
-
-    page_object, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request,
-                  'recipes/pages/category.html',
-                  context={
-                      'recipes': page_object,
-                      'pagination_range': pagination_range,
-                      'title': f'{recipes[0].category.name} - Category │',
-                  })
 
 
 def recipe(request, id):
