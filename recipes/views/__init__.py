585d9c446baeebfbdf882cd8d7e353bb972b6095
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-28 10:46:28 -0300

    Adicionando JSONResponse para a API que usa ListView

--- recipes/views/__init__.py
+++ recipes/views/__init__.py
@@ -3,3 +3,4 @@ from .category_view import RecipeListViewCategory
 from .home_view import RecipeListViewBase, RecipeListViewHome
 from .search_view import RecipeListViewSearch
 from .recipe_detail_view import RecipeDetail
+from .json import RecipeListViewHomeApi
---
+++ recipes/views/json.py
@@ -0,0 +1,15 @@
+from .base_view import RecipeListViewBase
+from django.http import JsonResponse
+
+
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        return JsonResponse(
+            list(recipes_list),
+            safe=False
+        )
