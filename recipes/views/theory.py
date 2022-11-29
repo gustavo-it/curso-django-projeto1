06314c6085cf74e34245d947f3bceef6587b65d9
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-29 15:45:15 -0300

    Criando seu próprio Manager para o Model (objects)

--- recipes/views/theory.py
+++ recipes/views/theory.py
@@ -6,8 +6,7 @@ from ..models import Recipe
 
 class Theory(View):
     def get(self, *args, **kwargs):
-        recipes = Recipe.objects.all()
-        print(recipes[0].title)
+        recipes = Recipe.objects.get_published()
         return render(self.request, 'recipes/pages/theory.html', context={
             'recipes': recipes
         })
