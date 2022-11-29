96b99dda7b68d5f62d47308a4a62c8eb77ddccf5
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-29 10:10:17 -0300

    QuerySets do Django são preguiçosas - Só são executadas quando preciso do valor

--- recipes/views/theory.py
+++ recipes/views/theory.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from ..models import Recipe
+
 
 class Theory(View):
     def get(self, *args, **kwargs):
-        return render(self.request, 'recipes/pages/theory.html')
+        recipes = Recipe.objects.all()
+        print(recipes[0].title)
+        return render(self.request, 'recipes/pages/theory.html', context={
+            'recipes': recipes
+        })
