999cbcd185259c238ea0131b595b1997b2d4954e
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-11-30 14:44:07 -0300

    Tag app: usando models.ManyToManyField para Tags

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.crypto import get_random_string
 from django.utils.text import slugify
-from django.contrib.contenttypes.fields import GenericRelation
+
 from tag.models import Tag
 
 
@@ -41,7 +41,7 @@ class Recipe(models.Model):
         Category, on_delete=models.SET_NULL, null=True, blank=True,
         default=None)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    tags = GenericRelation(Tag, related_query_name='recipes')
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
