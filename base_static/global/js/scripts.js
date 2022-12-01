bbb1dba66bf116743123517def4e77cc117a034a
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-01 11:32:55 -0300

    Criando a lógica de logout com JavaScript e form de POST

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -44,3 +44,15 @@ my_scope();
     buttonShowMenu.addEventListener('click', showMenu);
   }
 })();
+
+(() => {
+  const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+  const formLogout = document.querySelector('.form-logout');
+
+  for (const link of authorsLogoutLinks) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      formLogout.submit();
+    });
+  }
+})();
