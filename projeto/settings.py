900ef0ba41a6c5d066ad65fa06b9742a6c3c6056
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-06 09:36:49 -0300

    Instalando o Django Rest Framework + INSTALLED_APPS

--- projeto/settings.py
+++ projeto/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 from django.contrib.messages import constants
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "recipes",
     "authors",
     "debug_toolbar",
