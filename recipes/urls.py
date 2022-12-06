4ed75c96c828673a018e0872f3235c5f0fc28c7c
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-06 11:37:11 -0300

    Utilizando um serializer para list e detail com o Django Rest Framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -19,5 +19,7 @@ urlpatterns = [
     path('recipes/api/v1/<int:pk>/', views.RecipeDetailApi.as_view(),
          name='recipe_api_v1_detail'),
     path('recipes/theory/', views.Theory.as_view(), name='theory'),
-    path('recipes/api/v2/', views.recipe_api_list, name='recipes_api_v2')
+    path('recipes/api/v2/', views.recipe_api_list, name='recipes_api_v2'),
+    path('recipes/api/v2/<int:pk>/', views.recipe_api_detail,
+         name='recipes_api_v2_detail'),
 ]
