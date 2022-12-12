16676fb713dbcfb5d088f5b53589f5d0000e01ba
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-12 10:16:28 -0300

    Criando Authors e entendendo a segurança da API com Django Rest Framework (DRF)

--- authors/views/__init__.py
+++ authors/views/__init__.py
@@ -4,3 +4,4 @@ from .login_view import login_create, login_view
 from .logout_view import logout_view
 from .profile import ProfileView
 from .register_view import register_create, register_view
+from .api import *
