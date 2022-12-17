3370c241a6f23788b8c3d38298d06aabe5e95636
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-17 12:22:36 -0300

    Adicionando os caminhos de STATIC_ROOT na url do projeto

--- projeto/urls.py
+++ projeto/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # O primeiro parâmetro é a variável MEDIA_URL, e o segundo é o seu valor
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
