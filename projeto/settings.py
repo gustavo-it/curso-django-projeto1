299432e44e354a4a4361812dcc0662007a854553
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-21 14:01:41 -0300

    ALLOWED_HOSTS e CSRF_TRUSTED_ORIGINS - Parte 1

--- projeto/settings.py
+++ projeto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "INSECURE")
 DEBUG = True if os.environ.get("DEBUG") == "1" else False
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://recipes-teste.duckdns.org']
 
 # Application definition
 
