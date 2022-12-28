e5a87e6ae47581b50c30492a04c160d92f453a0a
gustavo-it <gustavo_castrosilva@yahoo.com>
2022-12-28 16:29:48 -0300

    Configurando as variáveis de ambiente no .env (dotenv) do Django Rest Framework

--- projeto/settings.py
+++ projeto/settings.py
@@ -185,9 +185,5 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-]
+CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CORS_ALLOWED_ORIGINS'))
